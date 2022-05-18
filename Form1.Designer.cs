dcc47654aeaef31a040d5b05250a96001e59afc0
tynoestylzie <chakaretinotenda@gmail.com>
2022-05-19 00:15:53 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿
+namespace QuizApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pctBx = new System.Windows.Forms.PictureBox();
+            this.lblQues = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pctBx)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pctBx
+            // 
+            this.pctBx.Image = global::QuizApp.Properties.Resources._15;
+            this.pctBx.Location = new System.Drawing.Point(1, 1);
+            this.pctBx.Name = "pctBx";
+            this.pctBx.Size = new System.Drawing.Size(784, 385);
+            this.pctBx.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pctBx.TabIndex = 0;
+            this.pctBx.TabStop = false;
+            this.pctBx.Click += new System.EventHandler(this.pctBx_Click);
+            // 
+            // lblQues
+            // 
+            this.lblQues.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblQues.Location = new System.Drawing.Point(1, 389);
+            this.lblQues.Name = "lblQues";
+            this.lblQues.Size = new System.Drawing.Size(784, 38);
+            this.lblQues.TabIndex = 1;
+            this.lblQues.Text = "Choose the approriate answer...";
+            this.lblQues.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1, 455);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(244, 68);
+            this.button1.TabIndex = 2;
+            this.button1.Tag = "1";
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.checkAnswerEvent);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(541, 455);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(244, 68);
+            this.button2.TabIndex = 3;
+            this.button2.Tag = "2";
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.checkAnswerEvent);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1, 571);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(244, 68);
+            this.button3.TabIndex = 5;
+            this.button3.Tag = "3";
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.checkAnswerEvent);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(541, 571);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(244, 68);
+            this.button4.TabIndex = 4;
+            this.button4.Tag = "4";
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.checkAnswerEvent);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(329, 661);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(112, 34);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "EXIT APP";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(787, 695);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblQues);
+            this.Controls.Add(this.pctBx);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pctBx)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pctBx;
+        private System.Windows.Forms.Label lblQues;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,274 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuizApp
+{
+    public partial class Form1 : Form
+    {
+
+        int correctAnswer;
+        int questionNum = 1;
+        int score;
+        int percentage;
+        int totalQues; 
+
+        public Form1()
+        {
+            InitializeComponent();
+            askQuestion(questionNum);
+
+            totalQues = 15; 
+        }
+
+        private void checkAnswerEvent(object sender, EventArgs e)
+        {
+            var senderObject = (Button)sender;
+            int buttonTag = Convert.ToInt32(senderObject.Tag); 
+            
+            if(buttonTag == correctAnswer)
+            {
+                score++; 
+            }
+
+            if(questionNum == totalQues)
+            {
+                percentage = (int)Math.Round((double)(score * 100) / totalQues);
+
+                MessageBox.Show("End of test!" + Environment.NewLine +
+                    "You have answered " + score + " questions correctly ." + Environment.NewLine +
+                    "Your total percentange is " + percentage + "%" + Environment.NewLine + 
+                    "Click OK to write test again");
+
+                score = 0;
+                questionNum = 0;
+                askQuestion(questionNum); 
+            }
+            questionNum++;
+            askQuestion(questionNum); 
+        }
+
+        private void askQuestion(int qnum)
+        {
+            switch (qnum)
+            {
+                case 1:
+
+                    pctBx.Image = Properties.Resources._1;
+                    lblQues.Text = "Click below button to answer the above question";
+
+                    button1.Text = "A";
+                    button2.Text = "B";
+                    button3.Text = "C";
+                    button4.Text = "D";
+
+                    correctAnswer = 3; 
+                    break;
+
+                case 2:
+
+                    pctBx.Image = Properties.Resources._2;
+                    lblQues.Text = "Click below button to answer the above question";
+
+                    button1.Text = "A";
+                    button2.Text = "B";
+                    button3.Text = "C";
+                    button4.Text = "D";
+
+                    correctAnswer = 2;
+                    break;
+
+                case 3:
+
+                    pctBx.Image = Properties.Resources._3;
+                    lblQues.Text = "Click below button to answer the above question";
+
+                    button1.Text = "A";
+                    button2.Text = "B";
+                    button3.Text = "C";
+                    button4.Text = "D";
+
+                    correctAnswer = 1;
+                    break;
+
+                case 4:
+
+                    pctBx.Image = Properties.Resources._4;
+                    lblQues.Text = "Click below button to answer the above question";
+
+                    button1.Text = "A";
+                    button2.Text = "B";
+                    button3.Text = "C";
+                    button4.Text = "D";
+
+                    correctAnswer = 3;
+                    break;
+
+                case 5:
+
+                    pctBx.Image = Properties.Resources._5;
+                    lblQues.Text = "Click below button to answer the above question";
+
+                    button1.Text = "A";
+                    button2.Text = "B";
+                    button3.Text = "C";
+                    button4.Text = "D";
+
+                    correctAnswer = 2;
+                    break;
+
+                case 6:
+
+                    pctBx.Image = Properties.Resources._6;
+                    lblQues.Text = "Click below button to answer the above question";
+
+                    button1.Text = "A";
+                    button2.Text = "B";
+                    button3.Text = "C";
+                    button4.Text = "D";
+
+                    correctAnswer = 4;
+                    break;
+
+                case 7:
+
+                    pctBx.Image = Properties.Resources._7;
+                    lblQues.Text = "Click below button to answer the above question";
+
+                    button1.Text = "A";
+                    button2.Text = "B";
+                    button3.Text = "C";
+                    button4.Text = "D";
+
+                    correctAnswer = 3;
+                    break;
+
+                case 8:
+
+                    pctBx.Image = Properties.Resources._8;
+                    lblQues.Text = "Click below button to answer the above question";
+
+                    button1.Text = "A";
+                    button2.Text = "B";
+                    button3.Text = "C";
+                    button4.Text = "D";
+
+                    correctAnswer = 3;
+                    break;
+
+                case 9:
+
+                    pctBx.Image = Properties.Resources._9;
+                    lblQues.Text = "Click below button to answer the above question";
+
+                    button1.Text = "A";
+                    button2.Text = "B";
+                    button3.Text = "C";
+                    button4.Text = "D";
+
+                    correctAnswer = 2;
+                    break;
+
+                case 10:
+
+                    pctBx.Image = Properties.Resources._10;
+                    lblQues.Text = "Click below button to answer the above question";
+
+                    button1.Text = "A";
+                    button2.Text = "B";
+                    button3.Text = "C";
+                    button4.Text = "D";
+
+                    correctAnswer = 4;
+                    break;
+
+                case 11:
+
+                    pctBx.Image = Properties.Resources._11;
+                    lblQues.Text = "Click below button to answer the above question";
+
+                    button1.Text = "A";
+                    button2.Text = "B";
+                    button3.Text = "C";
+                    button4.Text = "D";
+
+                    correctAnswer = 2;
+                    break;
+
+                case 12:
+
+                    pctBx.Image = Properties.Resources._12;
+                    lblQues.Text = "Click below button to answer the above question";
+
+                    button1.Text = "A";
+                    button2.Text = "B";
+                    button3.Text = "C";
+                    button4.Text = "D";
+
+                    correctAnswer = 3;
+                    break;
+
+                case 13:
+
+                    pctBx.Image = Properties.Resources._13;
+                    lblQues.Text = "Click below button to answer the above question";
+
+                    button1.Text = "A";
+                    button2.Text = "B";
+                    button3.Text = "C";
+                    button4.Text = "D";
+
+                    correctAnswer = 2;
+                    break;
+
+                case 14:
+
+                    pctBx.Image = Properties.Resources._14;
+                    lblQues.Text = "Click below button to answer the above question";
+
+                    button1.Text = "A";
+                    button2.Text = "B";
+                    button3.Text = "C";
+                    button4.Text = "D";
+
+                    correctAnswer = 4;
+                    break;
+
+                case 15:
+
+                    pctBx.Image = Properties.Resources._15;
+                    lblQues.Text = "Click below button to answer the above question";
+
+                    button1.Text = "A";
+                    button2.Text = "B";
+                    button3.Text = "C";
+                    button4.Text = "D";
+
+                    correctAnswer = 3;
+                    break;
+            }
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Close(); 
+        }
+
+        private void pctBx_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        /*private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }*/
+    }
+}
